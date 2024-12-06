cf5ea674ce2ec8728b96c8654d4acd270a843d8d
Sagarhaldar0009 <sagarhaldar987@gmail.com>
2024-12-07 01:46:49 +0530

    HomePage UI is Added using Internet

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,23 @@
 // import Image from "next/image";
 
+import ActionSection from "@/components/homepage/ActionSection";
+import ContactForm from "@/components/homepage/ContactForm";
+import FeatureSection from "@/components/homepage/FeatureSection";
+import BannerSection from "@/components/homepage/HomeBanner";
+import TestimonialSection from "@/components/homepage/TestimonialSection";
+
 export const metadata = {
   title : "Home - Task Manager"
 }
 
 const Home = () => {
   return (
-    <div className="text-center">
-      <div className="text-center text-2xl text-blue-500 m-4">
-          <>This is the Home Page</>
-          <br/>
-          Welcome to Next.js with Tailwind CSS!
-      </div>
-      
+    <div>
+      <BannerSection/>
+      <FeatureSection/>
+      <ActionSection/> 
+      <TestimonialSection/>
+      <ContactForm/>
     </div>   
   );
 };
